2f507d917c3ed6d83d61183b56bd93b98de1cb68
Ando AI Kivilaid <85090112+kivilaid@users.noreply.github.com>
2023-10-15 05:11:19 +0300

    Add files via upload

---
+++ knowledge.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Welcome to the KNOWLEDGE app!")
+
+if st.button("Click me"):
+    st.write("You clicked the button")
+
+st.sidebar.title("Sidebar")
+
+st.sidebar.markdown("This is a sidebar")
+    st.write("You clicked the button")
+
+
+
